13d2657cd5bab6be4b8f20b1bb99a45b98c0af3f
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-24 12:28:27 -0700

    created pokecard for each pokemon

--- index.js
+++ index.js
@@ -16,10 +16,29 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
     const response = await fetch(url);
     const pokemon = await response.json();
-    console.log(pokemon);
+    // console.log(pokemon);
+    createPokemonCard(pokemon);
   } catch (error) {
     console.log(error);
   }
 };
 
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+  //   console.log(pokemonEl);
+  const pokemonInnerHTML = `
+  <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+   </div>
+   <div class="info">
+        <span class="number">${pokemon.id}</span>
+        <h3 class="name">${pokemon.name}</h3>
+        <small class="type">Type <span>${pokemon.types[0].type.name}</span></small>
+   </div>
+  `;
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
+  //   console.log(pokemonEl);
+};
 fetchPokemons();
