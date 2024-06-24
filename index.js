9fd3412415cc1eff09c1859680127af288adaeac
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-24 10:51:23 -0700

    fetchpokemon and getpokemon fn

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-async function getUsers() {
+// https://pokeapi.co/docs/v2#pokemon-section
+const poke_container = document.getElementById("poke-container");
+// console.log(poke_container);
+const pokemon_count = 150;
+
+const fetchPokemons = async () => {
+  for (let id = 1; id <= pokemon_count; id++) {
+    //call function that makes api call
+    await getPokemon(id);
+  }
+};
+
+const getPokemon = async (id) => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
-    console.log(users);
+    //api call to pokeapi
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    const response = await fetch(url);
+    const pokemon = await response.json();
+    console.log(pokemon);
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-getUsers();
+fetchPokemons();
