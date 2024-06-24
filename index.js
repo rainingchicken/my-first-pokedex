9a4d3416a3afdc1003f50c9cc9bcfa091a853eb9
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-24 09:58:20 -0700

    added css

---
+++ index.js
@@ -0,0 +1,11 @@
+async function getUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    console.log(users);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUsers();
